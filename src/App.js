e5eef27ebc2b41b23b72abe87318dbc78e622c94
Ameya Keskar <ameyakeskar1999@gmail.com>
2019-12-26 23:08:24 +0530

    added animation successfully :smiley:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import FlareComponent from 'flare-react';
+import crab from "./crabHasADeadline.flr";
+class flareController extends FlareComponent.Controller {
+  initialize(artboard) {
+    if (artboard.name === "Artboard") {
+      this.Art = artboard;
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <FlareComponent width={500} height={500} animationName="Work" file="crabHasADeadline.flr"/>
-    </div>
-  )
+  advance(artboard, elapsed) {
+    return true;
+  }
 }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      myFlareController: new flareController()
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <FlareComponent
+          width={1024}
+          height={768}
+          animationName="Work"
+          file={crab}
+          controller={this.state.myFlareController}
+        />
+      </div>
+    );
+  }
+}
+
 
 export default App;
