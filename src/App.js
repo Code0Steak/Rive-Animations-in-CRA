aee7448d31721adcd6298d65de0a392db566ee6c
Ameya Keskar <ameyakeskar1999@gmail.com>
2019-12-26 22:59:14 +0530

    added Flare-Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FlareComponent from 'flare-react';
 function App() {
   return (
     <div>
-      
+      <FlareComponent width={500} height={500} animationName="Work" file="crabHasADeadline.flr"/>
     </div>
   )
 }
